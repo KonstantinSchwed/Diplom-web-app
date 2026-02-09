dd76c9120a8003b9b75cbfb6b3ca5d1010045e11
Kostya <kosti.schwedov@gmail.com>
2026-02-09 18:39:01 +0300

    end

--- server.js
+++ server.js
@@ -3191,6 +3191,7 @@ app.post('/logout', (req, res) => {
     res.clearCookie('authToken');
     res.json({ success: true });
 });
+// end game
 server.listen(port, '192.168.100.2', () => {
     console.log(`HTTP/WebSocket сервер доступен по адресу http://192.168.100.2:${port}`);
 });
