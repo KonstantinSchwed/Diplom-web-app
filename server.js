31dc14f7833697a956536dff82fc75a2e3325a8f
Kostya <kosti.schwedov@gmail.com>
2026-02-09 13:21:02 +0300

    Hello there 6

--- server.js
+++ server.js
@@ -3195,6 +3195,7 @@ app.post('/logout', (req, res) => {
 //hello there 3
 //hello there 4
 //hello there 5
+//hello there 6
 server.listen(port, '192.168.100.2', () => {
     console.log(`HTTP/WebSocket сервер доступен по адресу http://192.168.100.2:${port}`);
 });
