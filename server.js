ed2096b1cd2d616ce357668a24b9db6ef7cdca8d
Kostya <kosti.schwedov@gmail.com>
2026-04-08 14:12:29 +0300

    same

--- server.js
+++ server.js
@@ -3196,11 +3196,6 @@ app.post('/logout', (req, res) => {
 });
 //tests
 //test2
-//test3
-//test4 f
-//tes54 f
-
-
 server.listen(port, '192.168.100.2', () => {
     console.log(`HTTP/WebSocket сервер доступен по адресу http://192.168.100.2:${port}`);
-});git commit -am "Исправил баг в"
+});
